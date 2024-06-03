1035df064588dfdd848ad44e82ddbb51b21c1c39
excel10xx <michaelkenbenz@gmail.com>
2024-06-03 20:03:47 +0100

    updated-cors

--- src/server.js
+++ src/server.js
@@ -24,7 +24,13 @@ const path = require('path');
 const app = express();
 
 // Middleware
-app.use(cors());
+// Middleware
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    credentials: true 
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
